70b8f7c8d9ff939abcd23428dad5ea2df582cf8e
NilimaSardar <snilima181@gmail.com>
2024-08-12 19:32:10 +0545

    made responsive nav bar and landing page

--- js/script.js
+++ js/script.js
@@ -9,3 +9,21 @@ registerBtn.addEventListener('click',()=>{
 loginBtn.addEventListener('click',()=>{
     container.classList.remove("active");
 });
+
+
+
+function showSidebar(){
+    const sidebar = document.querySelector('.sidebar');
+    sidebar.style.display ='flex';
+}
+function hideSidebar(){
+    const sidebar = document.querySelector('.sidebar');
+    sidebar.style.display ='none';
+}
+
+function navigateTo(event, sectionId) {
+    event.preventDefault(); // Prevent the default link behavior
+    const section = document.querySelector(sectionId);
+    section.scrollIntoView({ behavior: 'smooth' }); // Scroll smoothly to the section
+    hideSidebar(); // Optionally hide the sidebar after navigation
+}
